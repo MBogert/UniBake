3c3e85a0cb615f305e78c28ebb2bb8aafc62d0d4
Matthew Bogert <Matthew Bogert>
2016-11-11 10:58:35 -0800

    Setting Up Database Schema

---
+++ Database/UniBake_DDL.sql
@@ -0,0 +1,59 @@
+.mode columns
+.headers on
+.nullvalue NULL
+PRAGMA foreign_keys = ON;
+
+
+create table UserLogin(
+	userID integer unique primary key, 
+	name text,
+	phone text unique
+);
+
+create table Login(
+	userID integer unique references UserLogin(userID) on update cascade on delete cascade,
+	email text primary key,
+	password text
+);
+
+create table School(
+	schoolID integer unique primary key,
+	name text,
+	domain text unique
+);
+
+create table Attends(
+	userID integer unique references UserLogin(userID) on update cascade on delete cascade,
+	schoolID integer references School(schoolID) on update cascade on delete cascade
+);
+
+create table Recipe(
+	filePath text unique primary key,
+	bakeTime integer
+);
+
+create table Category(
+	recipe text references Recipe(filePath) on update cascade on delete cascade,
+	category text,
+	primary key (filePath, category)
+);
+
+create table BakeRequest(
+	userID integer unique primary key references UserLogin(userID) on update cascade on delete cascade,
+	startTime time,
+	endTime time check (endTime > startTime) 
+);
+
+create table BakeRecipe(
+	userID integer references UserLogin(userID) on update cascade on delete cascade check(userID in BakeRequest),
+	recipe text references Recipe(filePath) on update cascade on delete cascade,
+	primary key (userID, filePath)
+);
+
+create table Pair(
+	user1 integer unique references UserLogin(userID) on update cascade on delete cascade check(userID in BakeRequest),
+	user2 integer unique references UserLogin(userID) on update cascade on delete cascade check(userID in BakeRequest),
+	recipe text references Recipe(filePath) on update cascade on delete cascade,
+	primary key (user1, user2)
+);
+
