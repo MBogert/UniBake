da10463a4b965012727056bd06215b398ec61b50
Matthew Bogert <Matthew Bogert>
2016-11-14 20:52:56 -0800

    Maybe this'll work

--- Database/UniBake_DDL.sql
+++ Database/UniBake_DDL.sql
@@ -5,48 +5,48 @@ PRAGMA foreign_keys = ON;
 
 
 create table UserLogin(
-	userID integer unique primary key, 
+	userID integer unique primary key on update cascade on delete cascade, 
 	name text,
 	phone text unique
 );
 
 create table Login(
-	userID integer unique references UserLogin(userID) on update cascade on delete cascade,
+	userID integer unique references UserLogin(userID),
 	email text primary key,
 	password text
 );
 
 create table School(
-	schoolID integer unique primary key,
+	schoolID integer unique primary key on update cascade on delete cascade,
 	name text,
 	domain text unique
 );
 
 create table Attends(
-	userID integer unique references UserLogin(userID) on update cascade on delete cascade,
-	schoolID integer references School(schoolID) on update cascade on delete cascade
+	userID integer unique references UserLogin(userID),
+	schoolID integer references School(schoolID)
 );
 
 create table Recipe(
-	filePath text unique primary key,
+	filePath text unique primary key on update cascade on delete cascade,
 	bakeTime integer
 );
 
 create table Category(
 	category text,
-	filePath text references Recipe(filePath) on update cascade on delete cascade,
+	filePath text references Recipe(filePath),
 	primary key (category, filePath)
 );
 
 create table BakeRequest(
-	userID integer unique primary key references UserLogin(userID) on update cascade on delete cascade,
+	userID integer unique primary key references UserLogin(userID),
 	startTime time,
 	endTime time check (endTime > startTime) 
 );
 
 create table RequestCategory(
-	userID integer references UserLogin(userID) on update cascade on delete cascade,
-	category text references Category(category) on update cascade on delete cascade,
+	userID integer references UserLogin(userID),
+	category text references Category(category),
 	primary key (userID, category)
 );
 
@@ -59,9 +59,9 @@ create table BakeRecipe(
 */
 
 create table Pair(
-	user1 integer unique references UserLogin(userID) on update cascade on delete cascade,
-	user2 integer unique references UserLogin(userID) on update cascade on delete cascade,
-	recipe text references Recipe(filePath) on update cascade on delete cascade,
+	user1 integer unique references UserLogin(userID),
+	user2 integer unique references UserLogin(userID),
+	recipe text references Recipe(filePath),
 	primary key (user1, user2)
 );
 
