fe749e89465cd603d16d480428ec864f1b5d8e18
Mark <mgilbert311@gmail.com>
2016-11-21 10:43:03 -0800

    The logInPage stuff with the cookies

---
+++ FormHandling/LoginPage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<h2>Enter username and password</h2>
+   
+        <h4><font color='red'>* required field.</font><br/></h4>
+
+          <form action="LoginPageHandle.php" method="post">
+			<table border="1">
+			<tr>
+        <td>UserID:<font color="red">*</font></td>
+        <td><input type ="integer" name="userID" required=""/> <br/></td>
+</tr>
+		<tr>
+        <td>Password:<font color="red">*</font></td>
+        <td><input type ="text" name="password" required=""/> <br/></td>
+</tr>
+<tr>
+<tr>
+</table>
+                <div align="center"><input type="submit"/></div>
+        </form>
+
+
+</body>
+</html>
+
+<html>
+<body>
+
+
+</body>
+</html>
