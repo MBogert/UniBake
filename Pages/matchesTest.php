27d5bc04613eccb4ce24ffe1af064235a3295c8e
MBogert <mgilbert311@gmail.com>
2016-11-26 13:20:29 -0800

    Matches algorithm with compare, need to test

--- Pages/matchesTest.php
+++ Pages/matchesTest.php
@@ -1,5 +1,33 @@
 <!DOCTYPE html>
 <?php
+//Pass in their preference lists
+//List2 should be an associative array with the userID and their preference list
+// function compare($list1, $list2){
+//     //Hashmap of users to the amount of matches they have
+//     $users = array();
+//     for($i=0; $i<count($list1); $i++){
+//         //The counter that says how many matches they have
+//         $counter =0;
+//         for($j=0; $j<count($list2); $j++){
+//             if($list1[$i] = $list2[$i]){
+//             $counter++;
+//             }   
+//     }
+//     array_push($userID, $counter)
+//     }
+
+    function compare2($list1, $list2){
+    //Hashmap of users to the amount of matches they have
+    $counter =0;
+    for($i=0; $i<count($list1); $i++){
+        //The counter that says how many matches they have
+
+            if($list1[$i] = $list2[$i]){
+            $counter++;
+            }   
+    }
+return $counter;
+    }
 
 //find the school that the student belongs to 
 try{
@@ -16,25 +44,30 @@ try{
                 $prepared->bindParam(':inputUserID', $_POST[userID]);
                 $result=$prepared->execute();
 
+                $matched = array();
                 foreach($result as $tuple){
                     //$data = array($tuple);
-                    $userID = $tuple[0];
+                    $userNum = $tuple[0];
                     $start = $tuple[1];
-                    //Find the common categories
-                    $userData = $db->prepare("select * from BakeRequest Natural Join RequestCategory where ()")
-                }
+                    //Get the preference categories
+                    $userData1 = $db->query("select category from RequestCategory where (userID = $userNum)");
+                    $userData2 = $db->query("select category from RequestCategory where(userID = :inputUserID");
+                    //Store the number of similar categories
+                    $similar = compare2($userData1, $userData2);
+                    //Add the user and their similar categories to the array
+                    matched_push($userNum, $similar);
 
+
+                }
+                //Sort the matches from high to low
+                arsort($matched);
+                //Close the db
+                $db=null;
 
     }catch(PDOException $e){
                 die('Exception : '.$e->getMessage()); //die will quit the script immediate
               }
               //Find the students at the same school
-
-
-— Need php to sort the elements or another sql query to get their category preferences
-
-$prepared = $db->prepare(“select * from Category where (studentID = inputStudentID)”);
-$prepared->bindParam(‘:inputStudentID’, $_POST[userID]);
-?>
+header("Location: matched.php");
 
 </html>
