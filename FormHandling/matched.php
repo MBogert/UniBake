e3ccb1a9aba800ce4953b2b21bb29ef2c4757d07
MBogert <mgilbert311@gmail.com>
2016-11-27 14:16:15 -0800

    Fixing the matching algorithm

---
+++ FormHandling/matched.php
@@ -0,0 +1,29 @@
+<?php
+//do we know this user?
+if (isset($_COOKIE['email'])){
+ $email = $_COOKIE['email']; //get the value of the cookie from browser
+ $password = $_COOKIE['password'];
+ //logUserIn($email, $password);
+} else {
+ //don't know this person (or cookie expired)
+	echo $_COOKIE['email'];
+
+	echo "Don't know you!";
+}
+?>
+<html>
+   
+   <head>
+      <title>These are your matches </title>
+   </head>
+   
+   <body>
+      <h5>
+      <?php echo $_POST['result'];
+       ?>
+       	
+       </h5> 
+      <h2><a href = "LogOut.php">Sign Out</a></h2>
+   </body>
+   
+</html>
--- Pages/matchesTest.php
+++ Pages/matchesTest.php
@@ -22,7 +22,7 @@
     for($i=0; $i<count($list1); $i++){
         //The counter that says how many matches they have
 
-            if($list1[$i] = $list2[$i]){
+            if($list1[$i] == $list2[$i]){
             $counter++;
             }   
     }
@@ -37,30 +37,31 @@ try{
                 //This is the query that filters out feasible users that could be possible matches
                 $prepared = $db-> prepare("WITH FindSchool as (select schoolID from LogIn NATURAL JOIN Attends where (Attends.userID = :inputUserID)),
 									FindStudents as (select * from LogIn NATURAL JOIN FindSchool NATURAL JOIN Attends where (Attends.schoolID = FindSchool.schoolID))
-									select * from BakeRequest NATURAL JOIN FindStudents where (:inputStartTime >= startTime OR :inputEndTime <= endTime)");
+									select userID from BakeRequest NATURAL JOIN FindStudents where (:inputStartTime >= startTime OR :inputEndTime <= endTime)");
                 //Bind the parameters for SQL Injection
-                $prepared->bindParam(':inputStartTime', $_POST[startTime]);
-                $prepared->bindParam(':inputEndTime', $_POST[endTime]);
-                $prepared->bindParam(':inputUserID', $_POST[userID]);
+                $prepared->bindParam(':inputStartTime', $_POST['startTime']);
+                $prepared->bindParam(':inputEndTime', $_POST['endTime']);
+                $prepared->bindParam(':inputUserID', $_POST['userID']);
                 $result=$prepared->execute();
 
                 $matched = array();
+
                 foreach($result as $tuple){
                     //$data = array($tuple);
-                    $userNum = $tuple[0];
-                    $start = $tuple[1];
+                    $otherPerson = $tuple[:inputUserID];
                     //Get the preference categories
-                    $userData1 = $db->query("select category from RequestCategory where (userID = $userNum)");
-                    $userData2 = $db->query("select category from RequestCategory where(userID = :inputUserID");
+                    $userData1 = $db->query("select category from RequestCategory where (userID = $otherPerson)");
+                    $userData2 = $db->query("select category from RequestCategory where (userID = :inputUserID");
                     //Store the number of similar categories
                     $similar = compare2($userData1, $userData2);
                     //Add the user and their similar categories to the array
-                    matched_push($userNum, $similar);
-
+                    matched_push($otherPerson, $similar);
 
                 }
                 //Sort the matches from high to low
-                arsort($matched);
+                $finalArr = arsort($matched);
+                $_POST['result'] = $finalArr;
+                //Print out these results
                 //Close the db
                 $db=null;
 
