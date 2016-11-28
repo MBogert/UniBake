1f36a6dcfb132eb20a1b7662a6cb4edee627dd60
Matthew Bogert <Matthew Bogert>
2016-11-27 16:08:48 -0800

    Added categories to request

--- FormHandling/makeRequest.php
+++ FormHandling/makeRequest.php
@@ -14,14 +14,65 @@
 </style>
 <body>
 
+
+
+<?php
+
+	try{
+
+		//Open database
+		$db = new PDO('sqlite:./../Database/unibake.db');
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		//Obtain all possible categories
+		$stmt = "SELECT DISTINCT category FROM Category;";
+		$result = $db->query($stmt);
+
+		//Close database
+		$db = null;
+	}
+	catch(PDOException $e){
+
+			//Page Redirect
+			die('Exception: '.$e->getMessage());
+			//header("Location: ../Pages/error.html");
+
+	} 
+
+?>
+
+
+
 	<h2> Please submit your request for when you would like to bake </h2>
 	<div>
 		<form action="finalMatch.php" method="post">
 		<input type="hidden" name="userID" value="$_COOKIE[userID]"<br>
 		Start Time:<input type="time" name="startTime"<br>
 		End Time:<input type="time" name="endTime"<br>
+		Preference 1: <select name = "category1">
+							<?php 
+								foreach($result as $tuple){
+									echo "<option>$tuple['category']</option>";
+								}
+							?>
+					  </select>
+		Preference 2: <select name = "category2">
+							<?php 
+								foreach($result as $tuple){
+									echo "<option>$tuple['category']</option>";
+								}
+							?>
+					  </select>
+		Preference 3: <select name = "category3">
+							<?php 
+								foreach($result as $tuple){
+									echo "<option>$tuple['category']</option>";
+								}
+							?>		
+					  </select>
 		<input type="submit">
 		</form>
 	</div>
 
 </body>
+
