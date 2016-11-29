e51c5b9159259b4f3c3a77de1d583a07f695af9e
MBogert <mgilbert311@gmail.com>
2016-11-28 19:51:05 -0800

    Testing out make request with insert query

--- FormHandling/finalMatch.php
+++ FormHandling/finalMatch.php
@@ -53,16 +53,20 @@ try{
                 echo "This is category3".$_POST['category3'];
 
                 $prepared1 = $db->prepare("Insert into RequestCategory (userID, category) VALUES (:userID, :category1) ");
-                $prepared1->bindParam(':category1', $_POST['category1']);
                 $prepared1->bindParam(':userID', $_COOKIE['userID']);
+                $prepared1->bindParam(':category1', $_POST['category1']);
+                $prepared1->execute();
 
                 $prepared2 = $db->prepare("Insert into RequestCategory (userID, category) VALUES (:userID, :category2) ");
                 $prepared2->bindParam(':category2', $_POST['category2']);
                 $prepared2->bindParam(':userID', $_COOKIE['userID']);
+                $prepared2->execute();
+
 
                 $prepared3 = $db->prepare("Insert into RequestCategory (userID, category) VALUES (:userID, :category3) ");
                 $prepared3->bindParam(':category3', $_POST['category3']);
                 $prepared3->bindParam(':userID', $_COOKIE['userID']);
+                $prepared3->execute();
 
 
 
