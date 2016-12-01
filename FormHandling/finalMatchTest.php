f9516ef180499d8206ac70c5a28a4d24c01d64e7
MBogert <mgilbert311@gmail.com>
2016-12-01 14:05:21 -0800

    Making changes to query

--- FormHandling/finalMatchTest.php
+++ FormHandling/finalMatchTest.php
@@ -89,7 +89,7 @@ try{
                 $prepared = $db->prepare("WITH FindSchool as (select schoolID from LogIn NATURAL JOIN Attends where (Attends.userID = :inputUserID)),
 									FindStudents as (select userID from LogIn NATURAL JOIN FindSchool NATURAL JOIN Attends where (Attends.schoolID = FindSchool.schoolID)),
                                     RestrictPair as (select userID from Pair NATURAL JOIN FindStudents where (Pair.user1 != FindStudents.userID AND Pair.user2 != FindStudents.userID))
-									select distinct userID from BakeRequest NATURAL JOIN RestrictPair where (:inputStartTime <= endTime AND :inputEndTime >= startTime AND userID != :inputUserID)");
+									select distinct userID from BakeRequest NATURAL JOIN RestrictPair where (:inputStartTime <= endTime OR :inputEndTime >= startTime AND userID != :inputUserID)");
 
 
                 //Bind the parameters for SQL Injection
