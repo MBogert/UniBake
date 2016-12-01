a795f748c74b15a57f644afc1e0eaf79aabe938e
rubinds <drubin@pugetsound.edu>
2016-12-01 14:38:47 -0800

    imgsrc

--- FormHandling/mfA.php
+++ FormHandling/mfA.php
@@ -217,7 +217,7 @@ try{
                                    </ul>
                               </div>
                               <div class=\"col-md-6\">
-                                  <img src=\"/AvatarImgs/gomer.png\">
+                                  <img src=\"./AvatarImgs/gomer.png\">
                                   <input type =\"hidden\" name=\"pairID\" value =" .$result['userID'].">
                                   <input type =\"hidden\" name=\"name\" value=" .$result['name'].">
                                   <input type =\"hidden\" name=\"email\" value=" .$result['email'].">
