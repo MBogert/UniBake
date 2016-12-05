66996e60a8e2ef010a7d51e6fed8baf4f6015cab
rubinds <drubin@pugetsound.edu>
2016-12-04 18:43:37 -0800

    thank you1

--- FormHandling/shinythankYou1.php
+++ FormHandling/shinythankYou1.php
@@ -4,71 +4,69 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+
   <meta charset="utf-8">
 <!-- Put in the stylesheet -->
-
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
   <title>Thank You</title>
-
-  <style>
-    body {
-      background-color: #cc0000;
-    }
-    img {
-      height: 150px;
-      width: 150px;
-    }
-    .options-box {
-      background: #ff0000;
-      border: 1px solid #2e2e1f;
-      border-radius: 3px;
-      height: 100%;
-      line-height: 35px;
-      padding: 10px 10px 10px 10px;
-      text-align: left;
-      /*width: 340px;*/
-      /*width: 90%;*/
-      margin-top: 50px;
-      /*margin-left: 50px;*/
-    }
-    .container {
-      height: 100%;
-      position: relative;
-    }
-    .matchContainer {
-      background: #ff0000;
-      border: 1px solid #2e2e1f;
-      border-radius: 3px;
-      height: 100%;
-      line-height: 35px;
-      padding: 10px 30px 10px 10px;
-      text-align: left;
-      /*width: 340px;*/
-      /*width: 90%;*/
-      margin: auto;
-      /*margin-left: 50px;*/
-    }
-    .row{
-      background-color: #FAEBD7;
-      width: 95%;
-      position: center;
-      padding: 10px 10px 10px 10px;
-      /*padding-left: 200px;*/
-      margin: auto;
-    }
-    .col-md-6 {
-      border: 1px solid #2e2e1f;
-      border-radius: 2px;
-    }
-    h1 {
-      align="center";
-    }
-    * {
-     border: 1px dashed #0000FF;
-    }
-  </style>
-
+  <!-- Include the navbar file -->
+  <?php include 'navbar.php'; ?>
 </head>
+<style>
+  body {
+    background-color: #cc0000;
+  }
+  img {
+    height: 150px;
+    width: 150px;
+  }
+  .options-box {
+    background: #ff0000;
+    border: 1px solid #2e2e1f;
+    border-radius: 3px;
+    height: 100%;
+    line-height: 35px;
+    padding: 10px 10px 10px 10px;
+    text-align: left;
+    /*width: 340px;*/
+    /*width: 90%;*/
+    margin-top: 50px;
+    /*margin-left: 50px;*/
+  }
+  .container {
+    height: 100%;
+    position: relative;
+  }
+  .matchContainer {
+    background: #ff0000;
+    border: 1px solid #2e2e1f;
+    border-radius: 3px;
+    height: 100%;
+    line-height: 35px;
+    padding: 10px 30px 10px 10px;
+    text-align: left;
+    /*width: 340px;*/
+    /*width: 90%;*/
+    margin: auto;
+    /*margin-left: 50px;*/
+  }
+  .row{
+    background-color: #FAEBD7;
+    width: 95%;
+    position: center;
+    padding: 10px 10px 10px 10px;
+    /*padding-left: 200px;*/
+    margin: auto;
+  }
+  .col-md-6 {
+    border: 1px solid #2e2e1f;
+    border-radius: 2px;
+  }
+  /** {
+   border: 1px dashed #0000FF;
+  }*/
+
+</style>
 
 <?php
 //This code will pair the user, then delete their previous bake request and Request Category information
@@ -114,25 +112,8 @@ session_start();
 
 ?>
 <body>
-  <div class="container">
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="index.html">Cook4Two</a>
-        </div>
-        <ul class="nav navbar-nav">
-          <li><a href="index.html">Home</a></li>
-          <li  class="active"><a href="matches.html">My Matches</a></li>
-          <li><a href="recipies.html">Recipies</a></li>
-          <li><a href="bio.html">My Bio</a></li>
-          <li><a href="LogOut.php" style="margin-left:800px">Logout</a></li>
-        </ul>
-      </div>
-    </nav>
-  </div>
-
-	<h1 >Thank you for baking with us</h1><br/>
+	<h1 align="center">Thank you for baking with us</h1><br/>
 	<?php
 		echo "<a href ='../Recipes/$_GET[filePath]' download>Download</a>";
 	?>
