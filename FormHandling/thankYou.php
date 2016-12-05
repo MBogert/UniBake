fb93de27f7e86cb1cc1e4b5ce22453a0a4d04420
rubinds <drubin@pugetsound.edu>
2016-12-04 19:11:49 -0800

    thank you1

--- FormHandling/thankYou.php
+++ FormHandling/thankYou.php
@@ -7,83 +7,11 @@ session_start();
 
   <meta charset="utf-8">
 <!-- Put in the stylesheet -->
-<!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" rel="stylesheet">
+
   <title>Thank You</title>
   <!-- Include the navbar file -->
   <?php include 'navbar.php'; ?>
 </head>
-<style>
-  body {
-    background-color: #cc0000;
-  }
-  img {
-    height: 150px;
-    width: 150px;
-  }
-  .options-box {
-    background: #ff0000;
-    border: 1px solid #2e2e1f;
-    border-radius: 3px;
-    height: 100%;
-    line-height: 35px;
-    padding: 10px 10px 10px 10px;
-    text-align: left;
-    /*width: 340px;*/
-    /*width: 90%;*/
-    margin-top: 50px;
-    /*margin-left: 50px;*/
-  }
-  .container {
-    height: 100%;
-    position: relative;
-  }
-  .matchContainer {
-    background: #ff0000;
-    border: 1px solid #2e2e1f;
-    border-radius: 3px;
-    height: 100%;
-    line-height: 35px;
-    padding: 10px 30px 10px 10px;
-    text-align: left;
-    /*width: 340px;*/
-    /*width: 90%;*/
-    margin: auto;
-    /*margin-left: 50px;*/
-  }
-  .row{
-    background-color: #FAEBD7;
-    width: 95%;
-    position: center;
-    padding: 10px 10px 10px 10px;
-    /*padding-left: 200px;*/
-    margin: auto;
-  }
-  .col-md-6 {
-    border: 1px solid #2e2e1f;
-    border-radius: 2px;
-  }
-  /** {
-   border: 1px dashed #0000FF;
-  }*/
-
-</style>
-<div class="container">
-
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container-fluid">
-      <div class="navbar-header">
-        <a class="navbar-brand" href="index.html">Cook4Two</a>
-      </div>
-      <ul class="nav navbar-nav">
-        <li><a href="index.html">Home</a></li>
-        <li  class="active"><a href="matches.html">My Matches</a></li>
-        <li><a href="recipies.html">Recipies</a></li>
-        <li><a href="bio.html">My Bio</a></li>
-        <li><a href="LogOut.php" style="margin-left:800px">Logout</a></li>
-      </ul>
-    </div>
-  </nav>
-</div> -->
 
 <?php
 //This code will pair the user, then delete their previous bake request and Request Category information
@@ -137,4 +65,3 @@ session_start();
 	<br/><a href = "HomePageTest.php">Return Home</a>
 
 </body>
-</html>
